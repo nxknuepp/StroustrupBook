db2569d7f60a2562ed51644ea3b04bdf473090e6
Nicholas Knueppel <nick@knueppel.com>
2023-03-13 12:21:26 -0700

    added nameage.cpp

---
+++ nameage.cpp
@@ -0,0 +1,12 @@
+// #include "std_lib_facilities.h"
+#include <iostream>
+using std::cin, std::cout, std::string;
+// input name and age, then print them out
+int main()
+{
+    string name;
+    int age;
+    cin >> name >> age;
+    cout << name << "  " << age << "\n";
+    return 0;
+}
